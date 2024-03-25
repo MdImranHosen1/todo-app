0a913b5eb19c01b3d4f1c84f48dfcb842ecaefb6
MdImranHosen1 <www.imransk.com@gmail.com>
2024-03-26 01:11:25 +0600

    "Local storage use for store data"

--- src/components/TodoEdit.jsx
+++ src/components/TodoEdit.jsx
@@ -101,3 +101,5 @@ export const TodoEdit = ({ todo }) => {
     </React.Fragment>
   );
 };
+
+
--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
+const initialData = [
     { id: 1, title: 'Buy groceries', content: 'Remember to buy milk and eggs', taskStatus: 'pending' },
     { id: 2, title: 'Walk the dog', content: 'Take Fido for a stroll around the park', taskStatus: 'completed' },
     { id: 3, title: 'Finish homework', content: 'Complete math assignment and submit it online', taskStatus: 'backlog' },
     { id: 4, title: 'Call mom', content: 'Discuss weekend plans and catch up on family news', taskStatus: 'pending' }
 ];
+
+const initialState = JSON.parse(localStorage.getItem("todos")) || initialData;
 
 const todosSlice = createSlice({
     name: 'todos',
@@ -13,7 +15,8 @@ const todosSlice = createSlice({
     reducers: {
         todoAdded: {
             reducer(state, action) {
-                state.push(action.payload);
+                state.unshift(action.payload);
+                localStorage.setItem("todos", JSON.stringify(state));
             },
             prepare(title, content, taskStatus) {
                 return {
@@ -26,14 +29,15 @@ const todosSlice = createSlice({
                 }
             }
         },
-        todoUpdated(state,action){
-            const { id, title, content, taskStatus }=action.payload;
-            const existingTodo=state.find(todo=>todo.id===id);
-            if(existingTodo){
-                existingTodo.title=title;
-                existingTodo.content=content;
-                existingTodo.taskStatus=taskStatus;
+        todoUpdated(state, action) {
+            const { id, title, content, taskStatus } = action.payload;
+            const existingTodo = state.find(todo => todo.id === id);
+            if (existingTodo) {
+                existingTodo.title = title;
+                existingTodo.content = content;
+                existingTodo.taskStatus = taskStatus;
             }
+            localStorage.setItem("todos", JSON.stringify(state));
         }
     }
 
